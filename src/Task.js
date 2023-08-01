24e4ddf36218fe95bda3e685543d0ca097683ec2
SIRAPARAPU MALLUNAIDU <114327222+mallunaidusiraparapu@users.noreply.github.com>
2023-08-01 14:41:47 +0530

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,13 @@
+// src/Task.js
+import React from 'react';
+
+const Task = ({ task }) => {
+  return (
+    <div className="task">
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+    </div>
+  );
+};
+
+export default Task;
---
+++ src/data.js
@@ -0,0 +1,26 @@
+// src/data.js
+const tasks = [
+    {
+      id: 1,
+      title: 'COMPONENTS ',
+      description: 'create all the required files in the components of src folder and export them',
+    },
+    {
+      id: 2,
+      title: 'ADD CSS',
+      description: 'Apply Different types of styles.',
+    },
+    {
+        id: 3,
+        title: 'RUN THE APPLICATION',
+        description: ' start the development server and see your task management app in action!',
+      },
+      {
+        id: 3,
+        title: 'INTEGRATE',
+        description: 'Integrate the frontend with backend using firebase',
+      },
+  ];
+  
+  export default tasks;
+  
